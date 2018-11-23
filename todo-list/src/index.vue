6c20c0919bb1611538f6952de51de856e4152f2a
windstormeye <877302410@qq.com>
2018-11-23 23:55:40 +0800

    [todo-list] init todo-list and add <text> font-family

---
+++ todo-list/src/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <image :src="logo" class="logo" />
+    <text class="iconfont">&#xe600;</text>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  },
+  beforeCreate () {
+    const domModule = weex.requireModule('dom')
+    domModule.addRule('fontFace', {
+      'fontFamily': 'iconfont',
+      'src': "url('http://at.alicdn.com/t/font_933576_hjux2fbay07.ttf')"
+    })
+  }
+}
+</script>
+
+<style scoped>
+  .iconfont {
+    font-family: iconfont;
+  }
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
